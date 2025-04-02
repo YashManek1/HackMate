f37946a45e819c1cdbf937681c56eea38155310e
Swarali Pednekar <swaralip1808@gmail.com>
2025-04-03 00:24:05 +0530

    Starting with Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import NotFound from './Pages/NotFound'
 import Home from './Pages/Landing/Home'
+import { Sidebar } from './Pages/Dashboard/Sidebar'
 
 const App = () => {
   return (
     <div className='min-h-screen max-w-screen m-0 p-0 overflow-x-hidden overflow-y-auto transition-all scroll-smooth'>
       <Routes>
         <Route path="/" element={<Home />} />
-        
+        <Route path="/dashboard" element={<Sidebar/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
