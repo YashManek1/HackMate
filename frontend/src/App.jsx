22ef7b2eaa0d42f155901499a9fb84c7ae576ec7
shahswayam01 <sswayam351@gmail.com>
2025-04-03 01:42:14 +0530

    routed for dashboard made successfully and sstarted with signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import NotFound from './Pages/NotFound'
 import Home from './Pages/Landing/Home'
-import { Sidebar } from './Pages/Dashboard/Sidebar'
+import DashApp from './Pages/Dashboard_/DashApp'
+import SignUp from "./Pages/Signup.jsx"
 
 const App = () => {
   return (
     <div className='min-h-screen max-w-screen m-0 p-0 overflow-x-hidden overflow-y-auto transition-all scroll-smooth'>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Sidebar/>} />
+        <Route path="/dashboard/*" element={<DashApp/>} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </div>
   )
