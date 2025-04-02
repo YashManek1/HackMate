1c3c8c5eaf3862a4a825862de63798954241c972
tanishshah <shahtanish207@gmail.com>
2025-04-03 03:23:04 +0530

    Landing Page

--- frontend/src/Pages/Landing/Home.jsx
+++ frontend/src/Pages/Landing/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Spline from '@splinetool/react-spline';
+import Header from './Header';
 
 const Home = () => {
   return (
-    <div>
-     
+    <div className='h-max w-full max-w-full p-4 flex items-center justify-around flex-row flex-wrap'>
+      <Header />
+      
+      <div className='h-max md:h-screen w-full flex items-start justify-between flex-row flex-wrap p-4 relative'>
+        
+        <div className='h-full w-[30%] flex items-center justify-center flex-col z-10'>
+          <h1 className='text-7xl'>HackMate</h1>
+          <p>Your AI </p>
+        </div>
+          
+        <Spline
+          scene="https://prod.spline.design/ZpPzN5pPmfNe64ny/scene.splinecode" 
+          className='absolute top-0 left-0 z-0'
+          />
+      </div>
     </div>
   )
 }
