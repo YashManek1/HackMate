bf4e529944673d81bc52eb42d531d7952d032b82
tanishshah <shahtanish207@gmail.com>
2025-04-02 21:55:08 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import NotFound from './Pages/NotFound'
+import Home from './Pages/Landing/Home'
+
+const App = () => {
+  return (
+    <div className='min-h-screen max-w-screen m-0 p-0 overflow-x-hidden overflow-y-auto transition-all scroll-smooth'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
