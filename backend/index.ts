e6cdf7fa9047934ff86d419fdef28eb9545bcb63
Yash Manek <yashmanek2001@gmail.com>
2025-04-06 02:45:46 +0530

    Minor changes

--- backend/index.ts
+++ backend/index.ts
@@ -9,7 +9,7 @@ import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import hackathonRoutes from "./routes/hackathonRoutes";
 import githubRoutes from "./routes/githubRoutes";
-import teamRoutes from './routes/teamRoutes';
+import teamRoutes from "./routes/teamRoutes";
 
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 import "./utils/passport";
@@ -46,7 +46,7 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/github", githubRoutes);
 app.use("/hackathon", hackathonRoutes);
-app.use('/api/teams', teamRoutes);
+app.use("/teams", teamRoutes);
 
 // Error handling
 app.use(errorMiddleware);
